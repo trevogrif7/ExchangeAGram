017f8497b8368df5aba2064b706601212595c594
trevogrif7 <trevogrif7@gmail.com>
2016-01-31 22:46:24 -0600

    Added FilterCell class

---
+++ ExchangeAGram/FilterCell.swift
@@ -0,0 +1,13 @@
+//
+//  FilterCell.swift
+//  ExchangeAGram
+//
+//  Created by Trevor Griffin on 1/31/16.
+//  Copyright © 2016 TREVOR E GRIFFIN. All rights reserved.
+//
+
+import UIKit
+
+class FilterCell: UICollectionViewCell {
+    
+}
